f93c15053b74af76c4f41f3f08d892f07c73408e
Fleurzz <121806759+Fleurzz@users.noreply.github.com>
2024-11-27 21:17:21 +0100

    Delete src directory

--- src/main/java/fr/nemesis07/ankhiastaff/commands/WarnCommand.java
+++
@@ -1,9 +0,0 @@
-package fr.nemesis07.ankhiastaff.commands;
-
-import fr.nemesis07.ankhiastaff.infractions.InfractionType;
-
-public class WarnCommand extends InfractionCommand {
-    public WarnCommand() {
-        super("warn", InfractionType.WARNING);
-    }
-}
